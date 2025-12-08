75219dcb71317d14f626f15f2eccb4e5ac6f52ff
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2025-12-08 14:46:43 +0300

    filter category

--- src/scripts/Tabs.js
+++ src/scripts/Tabs.js
@@ -3,7 +3,9 @@ class Tabs {
     selectors = {
         root: '[data-js-tabs]',
         btn: '[data-js-tabs-btn]',
-        list: '[data-js-tabs-list]'
+        list: '[data-js-tabs-list]',
+        filter: '[data-filter]',
+        card: '[data-category]'
     }
 
 
@@ -18,23 +20,42 @@ class Tabs {
         this.rootElement = document.querySelector(this.selectors.root)
         this.buttons = this.rootElement.querySelectorAll(this.selectors.btn)
         this.listElement = this.rootElement.querySelector(this.selectors.list)
+        this.filterButtons = this.rootElement.querySelectorAll(this.selectors.filter)
+        this.cards = document.querySelectorAll(this.selectors.card)
+        this.activeFilter = 'all'
         this.bindEvents()
     }
 
-    // при клике меняем состояние
+    // при клике меняем состояние и фильтруем
     onButtonClick = (event) => {
         const clickedBtn = event.currentTarget
-
+        // получаем data атрибут фильтра
+        const filterValue = clickedBtn.dataset.filter
         // снимаем активный класс с кнопок
         this.buttons.forEach(btn => {
             btn.classList.remove(this.stateClasses.isActive)
         })
-
         // Добавляем активный класс только нажатой кнопке
         clickedBtn.classList.add(this.stateClasses.isActive)
 
+        this.filterCards(filterValue)
 
     }
+    // фильтрация
+    filterCards(filter) {
+        this.activeFilter = filter
+        this.cards.forEach(card => {
+            // получаем категорию фильтра
+            const category = card.dataset.category
+
+            if (filter === 'all' || category === filter) {
+                card.style.display = 'block'
+            } else {
+                card.style.display = 'none'
+            }
+        })
+    }
+
     bindEvents() {
         this.buttons.forEach(btn => {
             btn.addEventListener('click', this.onButtonClick)
